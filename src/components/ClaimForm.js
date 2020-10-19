84259948429fa162274a2c9402b0c62c8156f49b
yijungai <ethan.yijun@gmail.com>
2020-10-19 19:11:02 +1100

    Format claim form page and wire up the claim title in the form with the api

--- src/components/ClaimForm.js
+++ src/components/ClaimForm.js
@@ -17,181 +17,175 @@ import Checkbox from "@material-ui/core/Checkbox";
 import { Layout } from "./Layout";
 
 const useStyles = makeStyles((theme) => ({
-    title: {
-        marginTop: 30,
+  title: {
+    marginTop: 30,
+  },
+  "@global": {
+    ul: {
+      margin: 0,
+      padding: 0,
+      listStyle: "none",
     },
-    "@global": {
-        ul: {
-            margin: 0,
-            padding: 0,
-            listStyle: "none",
-        },
-    },
-    step: {
-        width: "100%",
-        margin: 50,
-        alignItems: "center",
-    },
-    cardHeader: {
-        backgroundColor: theme.palette.type === "light" ?
-            theme.palette.grey[300] :
-            theme.palette.grey[700],
-    },
-    cardContent: {
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "baseline",
-        marginBottom: theme.spacing(2),
-        backgroundColor: theme.palette.type === "light" ?
-            theme.palette.grey[100] :
-            theme.palette.grey[700],
-    },
+  },
+  step: {
+    width: "100%",
+    margin: 50,
+    alignItems: "center",
+  },
+  cardHeader: {
+    backgroundColor:
+      theme.palette.type === "light"
+        ? theme.palette.grey[300]
+        : theme.palette.grey[700],
+  },
+  cardContent: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "baseline",
+    marginBottom: theme.spacing(2),
+    backgroundColor:
+      theme.palette.type === "light"
+        ? theme.palette.grey[100]
+        : theme.palette.grey[700],
+  },
 
-    paper: {
-        marginTop: 50,
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-    },
-    form: {
-        width: "100%", // Fix IE 11 issue.
-        marginTop: theme.spacing(1),
-    },
-    submit: {
-        margin: theme.spacing(3, 0, 2),
-        marginBottom: 300,
-    },
+  paper: {
+    marginTop: 50,
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+    marginBottom: 300,
+  },
 }));
 
-const tiers = [{
-        title: "Workflow 1",
-        description: ["Payment"],
-        buttonText: "Start",
-        buttonVariant: "outlined",
-    },
-    {
-        title: "Workflow 2",
-        description: ["Workflow2"],
-        buttonText: "Start",
-        buttonVariant: "outlined",
-    },
-    {
-        title: "Workflow 3",
-        description: ["Workflow3"],
-        buttonText: "Start",
-        buttonVariant: "outlined",
-    }
+const tiers = [
+  {
+    title: "Workflow 1",
+    description: ["Payment"],
+    buttonText: "Start",
+    buttonVariant: "outlined",
+  },
+  {
+    title: "Workflow 2",
+    description: ["Workflow2"],
+    buttonText: "Start",
+    buttonVariant: "outlined",
+  },
+  {
+    title: "Workflow 3",
+    description: ["Workflow3"],
+    buttonText: "Start",
+    buttonVariant: "outlined",
+  },
 ];
 
 export default function LoginForm() {
-    const [name, setName] = React.useState("");
-    const [amount, setAmount] = React.useState("");
-    const classes = useStyles();
-    const handleSubmit = async(event) => {
-        event.preventDefault();
-        const data = {
-            id: "573cf6e7-8caf-46f2-9805-bab6771066f2",
-            claim_title: "claim title test",
-            submitted_by: name,
-            amount: amount,
-            contract_id: "f8511f21-9d77-45c9-b0c3-fd0b2697c747",
-            principal_id: "3d48d94c-9b1f-451a-b6cb-690f87789c60",
-            superintendent_id: "9e8c8da9-4e47-4f7f-b32e-1628eddc5b8c",
-        };
-        const headers = {
-            "Content-Type": "application/json",
-            "Access-Control-Allow-Origin": "*",
-        };
-        let result = await axios.post(
-            "https://b8l77g698i.execute-api.eu-central-1.amazonaws.com/Stage/execution", { data },
-            headers
-        );
-        console.log(result.data);
-        alert("Payment claim has been submitted!");
+  const [name, setName] = React.useState("");
+  const [amount, setAmount] = React.useState("");
+  const [title, setTitle] = React.useState("");
+  const classes = useStyles();
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const data = {
+      id: "573cf6e7-8caf-46f2-9805-bab6771066f2",
+      claim_title: title,
+      submitted_by: name,
+      amount: amount,
+      contract_id: "f8511f21-9d77-45c9-b0c3-fd0b2697c747",
+      principal_id: "3d48d94c-9b1f-451a-b6cb-690f87789c60",
+      superintendent_id: "9e8c8da9-4e47-4f7f-b32e-1628eddc5b8c",
     };
-    return ( <
-        div >
-        <
-        Layout >
-        <
-        form onSubmit = { handleSubmit }
-        className = { classes.title } >
-        <
-        Typography component = "h1"
-        variant = "h2" >
-        Workflow System <
-        /Typography> <
-        Typography component = "h1"
-        variant = "h4" >
-        AS4000 <
-        /Typography> <
-        Container component = "main"
-        maxWidth = "xs" >
-        <
-        CssBaseline / >
-        <
-        div className = { classes.paper } >
-        <
-        form className = { classes.form }
-        noValidate >
-        <
-        TextField variant = "outlined"
-        margin = "normal"
-        required fullWidth label = "Name"
-        name = "name"
-        autoFocus type = "text"
-        id = "name"
-        value = { name }
-        onChange = {
-            (e) => setName(e.target.value) }
-        /> <
-        TextField variant = "outlined"
-        margin = "normal"
-        required fullWidth name = "amount"
-        label = "Amount"
-        type = "number"
-        id = "amount"
-        value = { amount }
-        onChange = {
-            (e) => setAmount(e.target.value) }
-        /> <
-        TextField variant = "outlined"
-        margin = "normal"
-        required fullWidth name = "claim title"
-        label = "Claim Title"
-        type = "text"
-        id = "claimTitle" /
-        >
-        <
-        /form> <
-        /div> <
-        /Container>
-
-        
-
-        <
-        Container component = "main"
-        maxWidth = "xs" >
-        <
-        CssBaseline / >
-        <
-        div className = "rem_sub" >
-        <
-        form className = { classes.form }
-        noValidate >
-        
-        <
-        Button type = "submit"
-        fullWidth variant = "contained"
-        color = "primary"
-        className = { classes.submit } >
-        Submit <
-        /Button> <
-        /form> <
-        /div> <
-        /Container> <
-        /form> <
-        /Layout> <
-        /div>
+    const headers = {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+    };
+    let result = await axios.post(
+      "https://b8l77g698i.execute-api.eu-central-1.amazonaws.com/Stage/execution",
+      { data },
+      headers
     );
+    console.log(result.data);
+    alert("Payment claim has been submitted!");
+  };
+  return (
+    <div>
+      <Layout>
+        <form onSubmit={handleSubmit} className={classes.title}>
+          <Typography component="h1" variant="h2">
+            Workflow System{" "}
+          </Typography>{" "}
+          <Typography component="h1" variant="h4">
+            AS4000{" "}
+          </Typography>{" "}
+          <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <div className={classes.paper}>
+              <form className={classes.form} noValidate>
+                <TextField
+                  variant="outlined"
+                  margin="normal"
+                  required
+                  fullWidth
+                  label="Name"
+                  name="name"
+                  autoFocus
+                  type="text"
+                  id="name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />{" "}
+                <TextField
+                  variant="outlined"
+                  margin="normal"
+                  required
+                  fullWidth
+                  name="amount"
+                  label="Amount"
+                  type="number"
+                  id="amount"
+                  value={amount}
+                  onChange={(e) => setAmount(e.target.value)}
+                />{" "}
+                <TextField
+                  variant="outlined"
+                  margin="normal"
+                  required
+                  fullWidth
+                  name="claim title"
+                  label="Claim Title"
+                  type="text"
+                  value={title}
+                  onChange={(e) => setTitle(e.target.value)}
+                  id="claimTitle"
+                />
+              </form>{" "}
+            </div>{" "}
+          </Container>
+          <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <div className="rem_sub">
+              <form className={classes.form} noValidate>
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  color="primary"
+                  className={classes.submit}
+                >
+                  Submit{" "}
+                </Button>{" "}
+              </form>{" "}
+            </div>{" "}
+          </Container>{" "}
+        </form>{" "}
+      </Layout>{" "}
+    </div>
+  );
 }
