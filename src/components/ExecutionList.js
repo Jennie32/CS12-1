0d9e797953d176136be3d6ff227cc2442efe62df
yijungai <ethan.yijun@gmail.com>
2020-10-20 09:14:49 +1100

    Fix dependency issue in useEffect and button variant

--- src/components/ExecutionList.js
+++ src/components/ExecutionList.js
@@ -17,6 +17,46 @@ export default function ExecutionList() {
     return "not found";
   }
   useEffect(() => {
+    const fetchExecutions = async (event) => {
+      const headers = {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+      };
+      let result = await axios.post(
+        "https://oys6sr3oo2.execute-api.eu-central-1.amazonaws.com/dev/execution",
+        {
+          maxResults: 20,
+          stateMachineArn:
+            "arn:aws:states:eu-central-1:638900115631:stateMachine:BasicWorkflow",
+        },
+        headers
+      );
+
+      var executionArns = result.data.executions.map(
+        (execution) => execution.executionArn
+      );
+      const asyncDescriptions = await Promise.all(
+        executionArns.map(async (arn) => {
+          return await axios.post(
+            "https://wid4bo7v0k.execute-api.eu-central-1.amazonaws.com/alpha/describeexecution",
+            { executionArn: arn.toString() },
+            headers
+          );
+        })
+      );
+      var titles = await Promise.all(
+        asyncDescriptions.map((description) =>
+          getTitle(JSON.parse(description.data.input))
+        )
+      );
+      var status = await Promise.all(
+        asyncDescriptions.map((description) => description.data.status)
+      );
+      var executions = titles.map(function (e, i) {
+        return [e, status[i]];
+      });
+      setexecutionList(executions);
+    };
     (async () => {
       await fetchExecutions();
       setloading(false);
@@ -27,46 +67,6 @@ export default function ExecutionList() {
     alert("clicked!");
   };
 
-  const fetchExecutions = async (event) => {
-    const headers = {
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*",
-    };
-    let result = await axios.post(
-      "https://oys6sr3oo2.execute-api.eu-central-1.amazonaws.com/dev/execution",
-      {
-        maxResults: 20,
-        stateMachineArn:
-          "arn:aws:states:eu-central-1:638900115631:stateMachine:BasicWorkflow",
-      },
-      headers
-    );
-
-    var executionArns = result.data.executions.map(
-      (execution) => execution.executionArn
-    );
-    const asyncDescriptions = await Promise.all(
-      executionArns.map(async (arn) => {
-        return await axios.post(
-          "https://wid4bo7v0k.execute-api.eu-central-1.amazonaws.com/alpha/describeexecution",
-          { executionArn: arn.toString() },
-          headers
-        );
-      })
-    );
-    var titles = await Promise.all(
-      asyncDescriptions.map((description) =>
-        getTitle(JSON.parse(description.data.input))
-      )
-    );
-    var status = await Promise.all(
-      asyncDescriptions.map((description) => description.data.status)
-    );
-    var executions = titles.map(function (e, i) {
-      return [e, status[i]];
-    });
-    setexecutionList(executions);
-  };
   return (
     <>
       <Layout>
@@ -88,7 +88,7 @@ export default function ExecutionList() {
                   <td>{item[1]}</td>
                   <td>
                     <div className="btn-container">
-                      <Button variant="primary" onClick={() => viewDetail()}>
+                      <Button variant="text" onClick={() => viewDetail()}>
                         View
                       </Button>
                     </div>
