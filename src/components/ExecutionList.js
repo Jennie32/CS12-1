46a2f7067871f9c6852fd79741e81aa6f3f06139
yijungai <ethan.yijun@gmail.com>
2020-10-24 22:36:29 +1100

    Link list page with detail page and refactor code

--- src/components/ExecutionList.js
+++ src/components/ExecutionList.js
@@ -3,10 +3,18 @@ import axios from "axios";
 import { Layout } from "./Layout";
 import Button from "@material-ui/core/Button";
 import ClipLoader from "react-spinners/ClipLoader";
+import { useHistory } from "react-router";
 
 export default function ExecutionList() {
   const [executionList, setexecutionList] = useState([]);
   const [loading, setloading] = useState(true);
+  const history = useHistory();
+  const headers = {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+  };
+  var dict = {};
+  var titles, status, descriptions, executionArns;
 
   async function getTitle(item) {
     if (item.hasOwnProperty("data")) {
@@ -16,65 +24,78 @@ export default function ExecutionList() {
     }
     return "not found";
   }
+
+  async function getARNs() {
+    let result = await axios.post(
+      "https://oys6sr3oo2.execute-api.eu-central-1.amazonaws.com/dev/execution",
+      {
+        maxResults: 20,
+        stateMachineArn:
+          "arn:aws:states:eu-central-1:638900115631:stateMachine:BasicWorkflow",
+      },
+      headers
+    );
+    var Arnresults = result.data.executions.map(
+      (execution) => execution.executionArn
+    );
+    return Arnresults;
+  }
+
+  async function getName(events) {
+    for (const event of events) {
+      if (
+        "stateEnteredEventDetails" in event &&
+        "name" in event.stateEnteredEventDetails
+      ) {
+        return event.stateEnteredEventDetails.name;
+      }
+    }
+    return "not found";
+  }
+
+  async function getDescriptions() {
+    const asyncDescriptions = await Promise.all(
+      executionArns.map(async (arn) => {
+        const tempDescription = await axios.post(
+          "https://wid4bo7v0k.execute-api.eu-central-1.amazonaws.com/alpha/describeexecution",
+          {
+            executionArn: arn.toString(),
+          },
+          headers
+        );
+
+        const executionHistories = await axios.post(
+          "https://prf7e0psi7.execute-api.eu-central-1.amazonaws.com/beta/execution",
+          {
+            executionArn: arn.toString(),
+            includeExecutionData: true,
+            maxResults: 10,
+            reverseOrder: true,
+          },
+          headers
+        );
+
+        const name = await getName(executionHistories.data.events);
+
+        return [
+          await getTitle(JSON.parse(tempDescription.data.input)),
+          tempDescription.data.status,
+          name,
+        ];
+      })
+    );
+    return asyncDescriptions;
+  }
+
   useEffect(() => {
     const fetchExecutions = async (event) => {
-      const headers = {
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*",
-      };
-      let result = await axios.post(
-        "https://oys6sr3oo2.execute-api.eu-central-1.amazonaws.com/dev/execution",
-        {
-          maxResults: 20,
-          stateMachineArn:
-            "arn:aws:states:eu-central-1:638900115631:stateMachine:BasicWorkflow",
-        },
-        headers
-      );
-
-      var executionArns = result.data.executions.map(
-        (execution) => execution.executionArn
-      );
-
-      const asyncDescriptions = await Promise.all(
-        executionArns.map(async (arn) => {
-          return await axios.post(
-            "https://wid4bo7v0k.execute-api.eu-central-1.amazonaws.com/alpha/describeexecution",
-            { executionArn: arn.toString() },
-            headers
-          );
-        })
-      );
-      var titles = await Promise.all(
-        asyncDescriptions.map((description) =>
-          getTitle(JSON.parse(description.data.input))
-        )
-      );
-      var status = await Promise.all(
-        asyncDescriptions.map((description) => description.data.status)
-      );
-
-      const events = (await Promise.all(
-        executionArns.map(async (arn) => {
-          return await axios.post(
-            "https://prf7e0psi7.execute-api.eu-central-1.amazonaws.com/beta/execution",
-            { executionArn: arn.toString(),includeExecutionData: true, maxResults: 10, reverseOrder: true },
-            headers
-          );
-        })
-      )).map(res => res.data.events);
-
-      const names = events.map(events => {
-        for (const event of events) {
-          if ("stateEnteredEventDetails" in event && 'name' in event.stateEnteredEventDetails) {
-            return event.stateEnteredEventDetails.name;
-          }
-        }
-        return 'not found';
-      })
-
-      var executions = titles.map(function (e, i) {
-        return [e, status[i], names[i]];
+      executionArns = await getARNs();
+      descriptions = await getDescriptions();
+      // const pairResults = await getTitlesAndStatus();
+      var executions = descriptions.map(function (e, i) {
+        let title, status, state;
+        [title, status, state] = e;
+        return [title, status, state];
       });
       setexecutionList(executions);
     };
@@ -85,7 +106,9 @@ export default function ExecutionList() {
   }, []);
 
   const viewDetail = (event) => {
-    alert("clicked!");
+    history.push(
+      "/execution-detail/arn:aws:states:eu-central-1:638900115631:execution:BasicWorkflow:ecaa9bcc-bbf9-4425-897d-a24312712b35"
+    );
   };
 
   return (
