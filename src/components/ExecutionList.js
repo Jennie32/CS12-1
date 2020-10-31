62ff9fe0e704819e0950f6428025cae04f866d9f
lyj306927107 <51026047+lyj306927107@users.noreply.github.com>
2020-10-31 18:14:11 +1100

    Update ExecutionList.js
    delete comments

--- src/components/ExecutionList.js
+++ src/components/ExecutionList.js
@@ -293,19 +293,19 @@ export default function ExecutionList() {
                 </tr>
               </thead>
               <tbody {...getTableBodyProps()}>
-                {// Loop over the table rows
+                {
                   rows.map(row => {
-                    // Prepare the row for display
+                   
                     prepareRow(row)
                     return (
-                      // Apply the row props
+                     
                       <tr {...row.getRowProps()}>
-                        {// Loop over the rows cells
+                        {
                           row.cells.map(cell => {
-                            // Apply the cell props
+                           
                             return (
                               <td {...cell.getCellProps()}>
-                                {// Render the cell contents
+                                {
                                   cell.render('Cell')}
                               </td>
                             )
